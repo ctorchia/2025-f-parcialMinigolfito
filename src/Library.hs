ab3b56e5463723d3250ec97c20661144fbb6042d
ctorchia <cristian_torchia@yahoo.com.ar>
2025-05-19 18:58:31 -0300

    Primer Commit

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,40 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+-- Modelo inicial
+data Jugador = UnJugador {
+  nombre :: String,
+  padre :: String,
+  habilidad :: Habilidad
+} deriving (Eq, Show)
+
+data Habilidad = Habilidad {
+  fuerzaJugador :: Number,
+  precisionJugador :: Number
+} deriving (Eq, Show)
+
+type Palo :: Jugador -> Tiro
+
+
+-- Jugadores de ejemplo
+bart = UnJugador "Bart" "Homero" (Habilidad 25 60)
+todd = UnJugador "Todd" "Ned" (Habilidad 15 80)
+rafa = UnJugador "Rafa" "Gorgory" (Habilidad 10 1)
+
+data Tiro = UnTiro {
+  velocidad :: Number,
+  precision :: Number,
+  altura :: Number
+} deriving (Eq, Show)
+
+type Puntos = Number
+
+-- Funciones útiles
+-- between n m x = elem x [n .. m]
+
+-- maximoSegun f = foldl1 (mayorSegun f)
+-- mayorSegun f a b
+--   | f a > f b = a
+--   | otherwise = b
+
