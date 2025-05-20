9b25bda81bc9808faf8ee2bd76cea07a143f0f4d
DESKTOP-V2BVTP7\CMT <cristian_torchia@yahoo.com.ar>
2025-05-19 21:07:58 -0300

    Hasta el punto 3 completo

--- src/Library.hs
+++ src/Library.hs
@@ -16,9 +16,6 @@ data Habilidad = Habilidad {
   precisionJugador :: Number
 } deriving (Eq, Show)
 
-type Palo :: Jugador -> Tiro
-
-
 -- Jugadores de ejemplo
 bart = UnJugador "Bart" "Homero" (Habilidad 25 60)
 todd = UnJugador "Todd" "Ned" (Habilidad 15 80)
@@ -32,8 +29,94 @@ data Tiro = UnTiro {
 
 type Puntos = Number
 
--- Funciones útiles
--- between n m x = elem x [n .. m]
+precisionDelJugador = precisionJugador . habilidad
+fuerzaDelJugador = fuerzaJugador . habilidad
+
+between n m x = elem x [n .. m]
+
+tiroPiola = UnTiro 100 100 0
+
+tiroFallido = UnTiro 0 0 0
+
+type Palo = Jugador -> Tiro
+
+putter :: Palo
+putter jugador = UnTiro {
+  velocidad = 10,
+  precision = ((*2) . precisionDelJugador) jugador ,
+  altura = 0
+}
+
+madera :: Palo
+madera jugador = UnTiro {
+  velocidad = 100,
+  precision = ((`div` 2) . precisionDelJugador) jugador ,
+  altura = 5
+}
+
+hierro :: Number -> Palo 
+hierro n jugador = UnTiro {
+  velocidad = ((*n) . fuerzaDelJugador) jugador,
+  precision = ((`div` n) . precisionDelJugador) jugador ,
+  altura = max 0 (n-3)
+}
+
+palos :: [Palo]
+palos = [putter, madera] ++ map hierro [1..10]
+
+golpe :: Jugador -> Palo -> Tiro
+golpe jugador palo = palo jugador
+
+type Obstaculo = Tiro -> Tiro
+
+
+obstaculo :: (Tiro -> Bool) -> (Tiro -> Tiro) -> Tiro -> Tiro
+obstaculo criterioSuperacion modificadorTiro tiro 
+  | criterioSuperacion tiro = modificadorTiro tiro
+  | otherwise = tiroFallido
+
+-- Opcion 1 --
+tunelConRampita :: Obstaculo
+tunelConRampita tiro =
+        obstaculo supera modificador tiro
+        where
+          supera tiro = precision tiro > 90 && altura tiro == 0
+          modificador tiro = UnTiro {
+            velocidad = ((*2) . velocidad ) tiro,
+            precision = 100,
+            altura = 0
+          }
+
+-- Opcion 2 --
+-- tunelConRampita :: Tiro -> Tiro
+-- tunelConRampita tiro 
+--         | precision tiro > 90 && altura tiro == 0 =
+--           UnTiro {
+--             velocidad = ((*2) . velocidad ) tiro,
+--             precision = 100,
+--             altura = 0
+--           }
+--         | otherwise = tiroFallido
+
+laguna :: Number -> Obstaculo
+laguna largo tiro
+        | velocidad tiro > 80 && between 1 5 (altura tiro) =
+            tiro {
+              altura = ((`div` largo) . altura) tiro
+            }
+        | otherwise = tiroFallido
+
+hoyo :: Obstaculo
+hoyo tiro
+        | between 5 20 (velocidad tiro) && altura tiro == 0 && precision tiro > 95 =
+            UnTiro 0 0 0
+        | otherwise = tiroFallido
+
+
+obstaculos = [hoyo, tunelConRampita, laguna 5]
+
+palosUtiles jugador obstaculo =
+      filter (\palo -> obstaculo (golpe jugador palo /= tiroFallido)) palos
 
 -- maximoSegun f = foldl1 (mayorSegun f)
 -- mayorSegun f a b
